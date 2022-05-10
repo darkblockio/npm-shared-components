d05a40c5c64971d83dca458877e3625104dbc9f8
roberto.torres <ts.roberto@gmail.com>
2022-05-10 13:37:22 +0100

    pdf, video and image stories

--- src/stories/Video.stories.js
+++ src/stories/Video.stories.js
@@ -5,20 +5,20 @@ import Panel from "../lib/Panel"
 import Player from "../lib/Player"
 import "../lib/db.css"
 
-const stories = storiesOf("Shared components states", module)
+const stories = storiesOf("Shared components - video", module)
 const state = {
-  value: "display",
+  value: "",
   context: {
     display: {
-      fileFormat: "encrypted(image/jpeg)",
-      fileSize: "size",
+      fileFormat: "encrypted(video/mp4)",
+      fileSize: "2.81 MB",
       arweaveTXLink: "arweaveTXLink",
-      arweaveTX: "arweaveTX",
+      arweaveTX: "eHRg9voGgrtgeV1odjprbjhExzEPJtaTmjPyN5m0zbA",
     },
   },
 }
 const mediaURL =
-  "https://gateway.darkblock.io/proxy?artid=09e30f8a-b51a-41a3-8be0-ffc21aa5ff44&session_token=1652179213863_gVlfGyhkDIocRdfpOQatjWcB8wQKyLFPj3sx2FYcCr4hav2vMttznUWVT6TdSWTpQ5%2B16vGdOqUYMzv%2BLoinDQ%3D%3D_Solana&token_id=28qnjJqxMeVJFRyNDp8yfGwd5DRsRYtoUnQncHWtyfRb&contract=&platform=Solana"
+  "https://gateway.darkblock.io/proxy?artid=265bac7d-4818-4431-ad4a-af2ed6aab7cf&session_token=1652185493005_0x5b8bc96fcf80cf519aaaa32099c0ffc7e4701fde0469cd2ae4c034403eea722f3ad4e1e01a9ed1718b959df262f5e1dc6393661ca88741ab0ccd4cbc46ab99c31b&token_id=30553606573219150352991292921105176340809048341686170040023897674790759038977&contract=0x495f947276749ce646f68ac8c248420045cb7b5e&platform=Ethereum"
 
 const config = {
   customCssClass: "", // pass here a class name you plan to use
