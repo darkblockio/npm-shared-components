4f601d654506c58f2a38f71abd304a7b44399221
Nicolas Diaz <nicolas.diaz@avvy.co>
2022-09-29 13:03:51 -0300

    Fixing naming conflicts in Stack

--- src/lib/Stack/detailModal.js
+++ src/lib/Stack/detailModal.js
@@ -9,47 +9,47 @@ const DetailModal = ({ db, open, onClose }) => {
     <>
       {open ? (
         <>
-          <div className="modal-container">
+          <div className="db-modal-container">
             <div
-              id="modal-bg"
+              id="db-modal-bg"
               onClick={() => onClose(true)}
             >
-              <div id="modal-box">
-                <div className="modal-box-container">
-                  <div id="modal-box">
-                    <div className="modal-first-row">
-                      <div className="modal-first-row-container"></div>
-                      <div className="modal-title">Details</div>
-                      <img className='cross-button' src="https://img.icons8.com/ios/50/000000/multiply.png"/>
+              <div id="db-modal-box">
+                <div className="db-modal-box-container">
+                  <div id="db-modal-box">
+                    <div className="db-modal-first-row">
+                      <div className="db-modal-first-row-container"></div>
+                      <div className="db-modal-title">Details</div>
+                      <img className='db-cross-button' src="https://img.icons8.com/ios/50/000000/multiply.png" />
                     </div>
-                    <hr className="my-4"/>
-                    <div className="detail-container">
-                      <div className="detail-darkblock-name">{db.name}</div>
-                      <div className="detail-darkblock-detail">{db.details}</div>
-                      <div className="detail-subtitle">Size: </div>
-                      <div className="detail-subtitle-text">{db.fileSize}</div>
-                      <div className="detail-subtitle">File Type: </div>
-                      <div className="detail-subtitle-text">{fileFormat}</div>
-                      <div className="detail-subtitle">
+                    <hr className="db-divider" />
+                    <div className="db-detail-container">
+                      <div className="db-darkblock-name">{db.name}</div>
+                      <div className="db-darkblock-detail">{db.details}</div>
+                      <div className="db-detail-subtitle">Size: </div>
+                      <div className="db-detail-subtitle-text">{db.fileSize}</div>
+                      <div className="db-detail-subtitle">File Type: </div>
+                      <div className="db-detail-subtitle-text">{fileFormat}</div>
+                      <div className="db-detail-subtitle">
                         Date Added:
                       </div>
-                      <div className="detail-subtitle-text">
+                      <div className="db-detail-subtitle-text">
                         {d.toLocaleString([], {
                           year: "numeric",
                           month: "numeric",
                           day: "numeric",
                         })}
                       </div>
-                        
-                      <div className="detail-subtitle">Arweave TX: </div>
-                      <div className="detail-subtitle-text">{db.arweaveTX}</div>
-                      
+
+                      <div className="db-detail-subtitle">Arweave TX: </div>
+                      <div className="db-detail-subtitle-text">{db.arweaveTX}</div>
+
                     </div>
-                    <div className="button-container">
-                        <button onClick={() => onClose(true)} className="done-button">
-                          Done
-                        </button>
-                      </div>
+                    <div className="db-button-container">
+                      <button onClick={() => onClose(true)} className="db-done-button">
+                        Done
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
