19f2dcef6acc5b60dd209813b92c62befe050d3f
Nicolas Diaz <nicolas.diaz@avvy.co>
2022-10-19 18:07:17 -0300

    fix font-weight & baseline to new db logo

--- src/lib/Footer/index.js
+++ src/lib/Footer/index.js
@@ -4,8 +4,10 @@ import StaticDBLogo from "../Panel/staticDBLogo"
 export default function FooterSharedComponents(config, state) {
   return (
     <div className="DarkblockWidget-Footer">
-      Powered by &nbsp;
-      <StaticDBLogo />
+      <p>Powered by &nbsp;</p>
+      <div>
+        <StaticDBLogo />
+      </div>
       {config.debug && <p>state: {state.value}</p>}
     </div>
   )
