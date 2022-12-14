86f3f75b0068fd1c119389aa3733736ad532e13b
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2022-12-14 14:01:05 -0500

    Fix upgrade modal

--- src/lib/Upgrader/fileUpload.js
+++ src/lib/Upgrader/fileUpload.js
@@ -34,34 +34,53 @@ const FileUpload = ({ fileState, setFileState }) => {
   return (
     <div className="Darkblock-fileupload-container">
       <div>
-        <h4 className="Darkblock-fileupload-header">{t('fileUpload')}</h4>
-        <p className="Darkblock-fileupload-header-supported">{t('supportedFileFormats')}:</p>
-        <p className="Darkblock-fileupload-formats">{formats.join(", ")}</p>
-        <p className="Darkblock-fileupload-info">{t('maxFileSize')}</p>
+        <h1 className="Darkblock-fileupload-header">{t("fileUpload")}</h1>
+        <h3 className="Darkblock-fileupload-header-supported">{t("supportedFileFormats")}:</h3>
+        <p className="Darkblock-fileupload-formats Darkblock-BodyText">{formats.join(", ")}</p>
+        <p className="Darkblock-fileupload-info Darkblock-BodyText">{t("maxFileSize")}</p>
         <div style={{ display: "flex", alignItems: "center" }} className="hidden">
           <span id="size" />
         </div>
       </div>
-      <div className="Darkblock-fileupload-input-container">
-        <input
-          id="file-upload" //maybe not change this one
-          type="file"
-          className="Darkblock-fileupload-button"
-          name="fileUpload"
-          accept={formats.join(" ")}
-          onChange={(e) => {
-            if (e.target.files) {
-              let f = e.target.files[0]
-              setFileState(f)
-            }
-          }}
-        />
-
-        <label htmlFor="Darkblock-file-upload">
-          {fileState && /\.(?=gif|jpg|png|jpeg)/gi.test(fileState.name) && (
-            <img src={URL.createObjectURL(fileState)} width="100px" height="100px" />
-          )}
-        </label>
+      <div className="Darkblock-BodyText Darkblock-fileupload-input-container">
+        <div className="Darkblock-file-upload-container">
+          <div className="Darkblock-file-upload-input">
+            <input
+              id="file-upload" //maybe not change this one
+              type="file"
+              className="Darkblock-input-file"
+              name="fileUpload"
+              accept={formats.join(" ")}
+              onChange={(e) => {
+                if (e.target.files) {
+                  let f = e.target.files[0]
+                  setFileState(f)
+                }
+              }}
+            />
+            <label id="Darkblock-file-select" htmlFor="file-upload">
+              Seleccionar Archivo
+            </label>
+          </div>
+        </div>
+        {!fileState ? (
+          <div className="Darkblock-file-upload-not-image-container">
+            <span>Ningún archivo selecionado</span>
+          </div>
+        ) : (
+          <div className="Darkblock-file-upload-image-container">
+            <div className="Darkblock-file-upload-image">
+              <label htmlFor="file-upload">
+                {fileState && /\.(?=gif|jpg|png|jpeg)/gi.test(fileState.name) && (
+                  <img src={URL.createObjectURL(fileState)} width="100px" height="100px" />
+                )}
+              </label>
+            </div>
+            <div className="Darkblock-file-upload-image-name">
+              <label htmlFor="file-upload">{fileState?.name}</label>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
--- src/lib/Upgrader/upgrade.modal.js
+++ src/lib/Upgrader/upgrade.modal.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { useTranslation } from "react-i18next"
+import Cross from "../Cross"
+import FooterUpgrader from "../FooterUpgrader"
 import UpgradeForm from "./upload.form"
 
 const UpgradeModal = ({ apiKey, state, open, onClose, authenticate, reset, dev }) => {
@@ -9,30 +11,28 @@ const UpgradeModal = ({ apiKey, state, open, onClose, authenticate, reset, dev }
       {open ? (
         <div className="Darkblock-upgrade-modal-container">
           <div id="Darkblock-upgrade-modal-bg">
-            <div id="Darkblock-upgrade-modal-box">
-              <div className="Darkblock-upgrade-modal-box-container">
-                <div id="Darkblock-upgrade-modal-box">
-                  <div className="Darkblock-upgrade-modal-first-row">
-                    <div className="Darkblock-upgrade-modal-first-row-container"></div>
-                    <div className="Darkblock-upgrade-modal-title">{t("upgrader.title")}</div>
-                    <img
-                      alt={"close"}
-                      className="Darkblock-upgrade-cross-button"
-                      onClick={() => onClose(true)}
-                      src="https://img.icons8.com/ios/50/000000/multiply.png"
-                    />
-                  </div>
-                  <hr className="Darkblock-upgrade-line-break" />
-                  <div className="Darkblock-upgrade-container">
-                    <UpgradeForm
-                      apiKey={apiKey}
-                      state={state}
-                      onClose={() => onClose(true)}
-                      authenticate={authenticate}
-                      reset={reset}
-                      dev={dev}
-                    />
-                  </div>
+            <div className="Darkblock-upgrade-modal-box-container">
+              <div id="Darkblock-upgrade-modal-box">
+                <div className="Darkblock-upgrade-modal-first-row">
+                  <div className="Darkblock-upgrade-modal-first-row-container"></div>
+                  <div className="Darkblock-upgrade-modal-title">{t("upgrader.title")}</div>
+                  <button className="Darkblock-upgrade-cross-button" onClick={() => onClose(true)}>
+                    <Cross />
+                  </button>
+                </div>
+                <hr className="Darkblock-upgrade-line-break" />
+                <div className="Darkblock-upgrade-container">
+                  <UpgradeForm
+                    apiKey={apiKey}
+                    state={state}
+                    onClose={() => onClose(true)}
+                    authenticate={authenticate}
+                    reset={reset}
+                    dev={dev}
+                  />
+                </div>
+                <div className="Darkblock-Upload-Footer">
+                  <FooterUpgrader />
                 </div>
               </div>
             </div>
